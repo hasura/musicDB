6e9932440affd1080c3543a7652a9aba70ab20e1
Tirumarai Selvan <tirumarai.selvan@gmail.com>
2017-03-09 16:34:48 +0530

    search functionality

--- app/js/directives.js
+++
@@ -1,7 +0,0 @@
-app.directive("mdbHeader", function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/partials/header.html'
-    }
-
-});
--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,8 @@ var app = window.app = angular.module('musicdb', [
   'ngRoute'
 ]);
 
+var projectName = window.projectName = 'carve65';
+
 /**
  * Configure the Routes
  */
@@ -9,15 +11,53 @@ app.config(['$routeProvider','$locationProvider', function ($routeProvider, $loc
   $locationProvider.hashPrefix('');
   $routeProvider
     // Home
-    .when("/", {templateUrl: "app/partials/home.html", controller: "PageCtrl"})
+    .when("/search", {templateUrl: "app/partials/searchResults.html", controller: 'SearchCtrl' })
 }]);
 
 /**
  * Controls the page
  */
-app.controller('PageCtrl', ['$scope', function ($scope) {
+app.controller('PageCtrl', ['$scope', '$location', function ($scope, $location) {
 
   console.log("PageCtrl loaded");
-  console.log($scope.searchQuery);
+
+  $scope.searchArtist = function(artist) {
+    console.log("inside searchArtist");
+    $location.url('/search?query=' + artist);
+  }
+
+}]);
+
+app.controller('SearchCtrl', ['$scope', '$location', '$http', function( $scope, $location, $http) {
+
+  console.log($location.search().query);
+  var query = $location.search().query;
+
+  var req = {
+    method: 'POST',
+    url: 'https://data.' + projectName + '.hasura-app.io/v1/query',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      type: "select",
+      args: {
+        "table": "artist",
+        "columns": ["*.*"],
+        "where": {"name": {"$like": "%"+query+"%" }},
+        "limit": 10
+      }
+    }
+  }
+
+  $http(req).then(
+    function success(data){
+      console.log(data);
+
+    },
+
+    function error(data){
+      console.log(data);
+    });
 
 }]);
